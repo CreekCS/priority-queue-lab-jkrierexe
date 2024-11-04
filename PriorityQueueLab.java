f9838f890a29878677209a25b87f93028195442c
Jackson Krier <jackson.krier@gmail.com>
2024-11-04 20:42:26 +0000

    working on it

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,16 @@
 // import statements
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class PriorityQueueLab {
-  
-}
-
+    public static void main(String[] args){
+        File myObj = new File("RandIntegers.txt");
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        Scanner scanner = new Scanner(myObj);
+        while (scanner.hasNext()) {
+            arr.add(scanner.nextLine());
+        }
+        scanner.close();
+    }
